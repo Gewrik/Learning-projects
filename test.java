7c1e0e635a0df5c712ac6fbe8f02cefa2b6da992
avatar <90562588+Gewrik@users.noreply.github.com>
2022-07-08 17:30:28 +0300

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 
-/* 
-Programm that reads 3 numbers from User - start, end and multiple - and checks if 
-any numbers between Start (inclusive) and End (not inclusive) are multiple of number 
-Multiple, and then finds their total sum
-*/
+
 
 
 public class test {
