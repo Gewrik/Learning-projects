548c0676c580fe3f38a3ddab1a8b185cf130a853
Gewrik <babushkinns@gmail.com>
2022-09-14 14:54:35 +0300

    Temp

--- Main.java
+++ Main.java
@@ -5,59 +5,22 @@ import java.util.Objects;
 public class Main {
 
     public static void main(String[] args) {
-        String[] memory = {"object15", null, null, "object2", "object3", null, null, "object32", null, "object4", null, null, null, "object5"};
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
         executeDefragmentation(memory);
         System.out.println(Arrays.toString(memory));
+
 
     }
 
     public static void executeDefragmentation(String[] array) {
 
-
-
-        System.out.println("*********0 cycle***********");
-        System.out.println("***************************");
-        System.out.println();
-        System.out.print("array:");
-        System.out.println(Arrays.toString(array));
-        System.out.println();
-
-        String[] arrayNew = Arrays.copyOf(array, array.length);
-        int j = arrayNew.length - 1;
-        for (int i = array.length - 1; i >= 0; i--) {
-            if (array[i] == null) {
-                arrayNew[j] = null;
-                j--;
+        for (int out = array.length - 1; out >= 1; out--) {
+            for (int in = 0; in < out; in++) {
+                if (array[in] > array[in + 1])
             }
         }
-        j = 0;
-
-        System.out.println("*********1 cycle***********");
-        System.out.print("arrayNew:");
-        System.out.println(Arrays.toString(arrayNew));
-        System.out.println();
-        System.out.println("***************************");
-        System.out.println();
-        System.out.print("array:");
-        System.out.println(Arrays.toString(array));
-
-
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != null) {
-                arrayNew[i] = array[j];
-                j++;
-            }
-        }
-        System.out.println("*********2 cycle***********");
-        System.out.print("arrayNew:");
-        System.out.println(Arrays.toString(arrayNew));
-        System.out.println();
-        System.out.println("***************************");
-        System.out.println();
-        System.out.print("array:");
-        System.out.println(Arrays.toString(array));
-
     }
-
 }
+
+
+
